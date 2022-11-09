943c5bf14bab66daad9ee86bef43f57dcb6d8ec8
Suyash Purwar <purwarsuyash06@gmail.com>
2022-11-10 05:15:23 +0530

    Made changes in the UserInfo table and add Swipes and Reports table

--- tables-create.sql
+++ tables-create.sql
@@ -15,15 +15,44 @@ CREATE TABLE Users (
 
 CREATE TABLE UserInfo (
     id INT PRIMARY KEY,
-    birthday DATE,
-    gender VARCHAR(255) CHECK(gender in ('MALE', 'FEMALE', 'OTHER')),
-    sexual_orientation VARCHAR(255),
-    show_profile_type VARCHAR(255) CHECK(show_profile_type in ('MALE', 'FEMALE', 'EVERYONE')),
-    age INT,
-    location_latitude FLOAT,
-    location_longitude FLOAT,
-    distance_range INT,
+    birthday DATE NOT NULL,
+    gender VARCHAR(255) NOT NULL CHECK(gender in ('MALE', 'FEMALE', 'OTHER')),
+    sexual_orientation VARCHAR(255) NOT NULL,
+    show_profile_type VARCHAR(255) NOT NULL CHECK(show_profile_type in ('MALE', 'FEMALE', 'EVERYONE')),
+    age INT NOT NULL CHECK(age >= 18),
+    location_latitude FLOAT NOT NULL,
+    location_longitude FLOAT NOT NULL,
+    distance_range INT DEFAULT 10,
     bio VARCHAR(255),
     pronouns VARCHAR(255),
     FOREIGN KEY(id) REFERENCES Users(id)
+);
+
+CREATE TABLE Swipes (
+    id INT,
+    swiped_by INT NOT NULL,
+    swiped_to INT NOT NULL,
+    swipe_type VARCHAR(255) NOT NULL CHECK(swipe_type in ('LIKE', 'SUPERLIKE')),
+    is_accepted INT DEFAULT 0,
+    is_blocked int DEFAULT 0,
+    swiped_on DATE NOT NULL,
+    blocked_on DATE,
+    PRIMARY KEY(id),
+    FOREIGN KEY(swiped_by) REFERENCES Users(id),
+    FOREIGN KEY(swiped_to) REFERENCES Users(id)
+);
+
+CREATE TABLE Reports (
+    id INT,
+    reporter_id INT NOT NULL,
+    reported INT NOT NULL,
+    remarks VARCHAR(255) NOT NULL,
+    report_date DATE NOT NULL,
+    is_resolved INT DEFAULT 0,
+    resolved_by VARCHAR(255),
+    resolve_date DATE,
+    resolve_remark VARCHAR(255),
+    PRIMARY KEY(id),
+    FOREIGN KEY(reporter_id) REFERENCES Users(id),
+    FOREIGN KEY(reported) REFERENCES Users(id)
 );
