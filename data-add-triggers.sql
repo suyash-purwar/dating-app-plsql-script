5c004518102226bc84d29974fcb3349c847db8d8
Suyash Purwar <purwarsuyash06@gmail.com>
2022-11-10 15:22:45 +0530

    Add trigger for swipe id creation and allocation. Push code for Swipes table data population

--- data-add-triggers.sql
+++ data-add-triggers.sql
@@ -1,4 +1,6 @@
 -- Responsible for automatically allocating id numbers to the user
+CREATE SEQUENCE user_id_seq;
+
 CREATE TRIGGER assign_user_id
 BEFORE INSERT ON Users
 FOR EACH ROW
@@ -6,4 +8,16 @@ BEGIN
 SELECT user_id_seq.nextval
 INTO :new.id
 FROM dual;
+END;
+
+-- Responsible for automatically allocating id number to the Swipe
+CREATE SEQUENCE swipe_id_seq;
+
+CREATE TRIGGER assign_swipe_id
+BEFORE INSERT ON Swipes
+FOR EACH ROW
+BEGIN
+SELECT swipe_id_seq.nextval
+INTO :new.id
+FROM dual
 END;
